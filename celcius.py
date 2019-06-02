8637466380aa4236d183000389b09e6e34e4881b
jeevakamal <50892880+jeevakamal@users.noreply.github.com>
2019-06-02 14:02:30 +0530

    Create celcius.py

---
+++ celcius.py
@@ -0,0 +1,9 @@
+t=input()
+sign=t[-1]
+t=int(t[:-1])
+if sign=='c'or sign =='c':
+  t=rount(t * (9/5) + 32)
+  print(str(t)+ 'f')
+elif sign =='f'or sign =='f':
+  t=round((t-32)*(5/9))
+  print(str(t) + 'c')
