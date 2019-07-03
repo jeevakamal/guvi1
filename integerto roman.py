d0e5001596d677c70c2fb315b8e53ae6922a8490
jeevakamal <50892880+jeevakamal@users.noreply.github.com>
2019-07-03 21:57:13 +0530

    Create integerto roman.py

---
+++ integerto roman.py
@@ -0,0 +1,5 @@
+x1=input()
+x2=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if(x1==x2[i]):
+    print(i)
